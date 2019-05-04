be7b204f05205608a98afc56ab6d4697af7aeab7
ggzhang0071 <155051843@qq.com>
2019-05-04 13:51:49 +0000

    PSOGSA recurrencePlots is ok

--- examples/TorchESN.py
+++ examples/TorchESN.py
@@ -7,10 +7,10 @@ from torchesn import utils
 import time
 from sklearn import preprocessing
 import matplotlib.pyplot as plt 
-import plot_recurrence as plr
+from pyts.image import RecurrencePlot
 import pandas as pd
 import DataLoad
-# from ipdb import set_trace
+# from ipdb import set_traceex
 
 def rec_plot(s, eps=0.001, steps=10):
     if eps==None: eps=0.1
@@ -57,25 +57,35 @@ def torch_ESN(parameters):
 
     model(trX, washout, None, trY_flat)
     model.fit()
-#     set_trace()
-    output, hidden = model(trX, washout)
+    output, hidden0 = model(trX, washout)
     
 #         print("Training error:", loss_fcn(output, trY[washout[0]:]).item())
 
             # Test
-    output, hidden = model(tsX, [0], hidden)
+    output, hidden = model(tsX, [0], hidden0)
     
-    if loss_fcn(output, tsY).item()<3e-10:
-        hiddenState=np.array(hidden.view(numlayers,hiddensize).tolist())
-        for i in range(numlayers):
-            plt.imshow(hiddenState)
-            hiddenState1=hiddenState[i]/np.mean(hiddenState[i])
-            rec = rec_plot(hiddenState1)
-            plt.imshow(rec, cmap = plt.cm.gray)
-            plt.savefig('../Results/RecurrencePlots'+str(numlayers)+'_'+str(i)+'_'+str(hiddensize)+'.png',dpi=600)
-            plt.show()
-
+    if loss_fcn(output, tsY).item()<1e-9:
+        hiddenState=np.array(hidden0.view(numlayers,hiddensize).tolist())
+        rp = RecurrencePlot(dimension=7, time_delay=3,
+                    threshold='percentage_points',
+                    percentage=30)
+        X_rp = rp.fit_transform(hiddenState)
+        plt.figure(figsize=(6, 6))
+        plt.imshow(X_rp[0], cmap='binary', origin='lower')
+#         plt.title('Recurrence Plot', fontsize=14)
+        plt.savefig('../Results/RecurrencePlots'+str(numlayers)+'_'+str(hiddensize)+'_'+str(loss_fcn(output, tsY).item())+'.png',dpi=600)
+        plt.show()
+        weightsName='reservoir.weight_hh'
+        for name, param in model.named_parameters():
+#             print(name,param)
+            if name.startswith(weightsName):
+#                 set_trace()
+                torch.save(param,'../Results/weights'+str(loss_fcn(output, tsY).item())+'.pt')
+                
     print("Test error:", loss_fcn(output, tsY).item())
+
+ 
+   
 #         print("Ended in", time.time() - start, "seconds.")
 #         preds=[]
 #         testD=[]
