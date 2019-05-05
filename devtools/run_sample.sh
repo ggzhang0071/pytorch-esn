0a0fc65a0a074eb1f4a9f9c580c5977ff6b318a2
Victor Li <gwli@outlook.com>
2019-05-05 09:03:52 +0800

    comment _esn

--- devtools/run_sample.sh
+++ devtools/run_sample.sh
@@ -4,8 +4,9 @@ cl=`git rev-parse HEAD|cut -c1-7`
 
 cd $TOP/examples
 
+rm -fr Results 
 rm -fr *.log
-python3 PSOGSA_ESN.py 2>&1 |tee esn_${cl}_$timestamp.log
-#python3 ESNRegularizationCompare.py 2>&1 |tee esn_regularizationCompare_${cl}_$timestamp.log
+#python3 PSOGSA_ESN.py 2>&1 |tee esn_${cl}_$timestamp.log
+python3 ESNRegularizationCompare.py 2>&1 |tee esn_regularizationCompare_${cl}_$timestamp.log
 
 
